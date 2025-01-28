fe1e545fd204d1ca15e0ca37ea31c62029af4f01
fisiofaz <75371386+fisiofaz@users.noreply.github.com>
2025-01-28 10:24:22 -0300

    corigido JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,18 @@
-document.getElementById('toggle-button').addEventListener('click', function() {
-    const textSection = document.getElementById('text-section');
-    const toggleIcon = document.getElementById('toggle-icon');
-    
-    if (textSection.style.display === 'none' || textSection.style.display === '') {
-        textSection.style.display = 'block'; 
-        toggleIcon.src = '../icon/SetaBaixo.svg'; 
-        toggleIcon.alt = 'Seta para baixo';
-        
-    } else {
-        textSection.style.display = 'none';
-        toggleIcon.src = '../icon/SetaLado.svg'; 
-        toggleIcon.alt = 'seta para o lado '; 
-    }
-});
 
+const toggleButtons = document.querySelectorAll('.toggle-button');
+
+toggleButtons.forEach((button) => {
+    button.addEventListener('click', function() {
+        const textSection = this.closest('section').querySelector('.idioma_texto');
+        const toggleIcon = this.querySelector('.toggle-icon');
+        if (textSection.classList.contains('hidden')) {
+            textSection.classList.remove('hidden');
+            toggleIcon.src = '../icon/SetaBaixo.svg';
+            toggleIcon.alt = 'Seta para baixo';
+        } else {
+            textSection.classList.add('hidden');
+            toggleIcon.src = '../icon/SetaLado.svg';
+            toggleIcon.alt = 'Seta para o lado';
+        }
+    });
+});
